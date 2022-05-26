0bd772648f3d8b2ccc1d85d9f5bfdc02bb195a98
mcrucovschii <mcrucovschii@gmail.com>
2022-05-26 19:54:55 +0200

    3

--- Output.tf
+++ Output.tf
@@ -6,3 +6,18 @@ output "webserver_instance_id" {
 output "webserver_public_dns" {
   value = aws_instance.WebServer.public_dns
 }
+output "data_aws_availability_zones" {
+  value = data.aws_availability_zones.working.names
+}
+
+output "data_aws_caller_identity" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "data_aws_regions_name" {
+  value = data.aws_region.current.name
+}
+
+output "aws_vpcs" {
+  value = data.vpcs.current.ids
+}
--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
 provider "aws" {
   region = "us-west-1"
   data "aws_availability_zone" "working" {}
+  data "aws_caller_identity" "current" {}
+  data "aws_region" "current" {}
+  data "aws_vpcs" "current" {}
 }
