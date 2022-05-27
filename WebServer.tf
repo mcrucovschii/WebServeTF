5e02551fd49c378a9e80451e41e9967e006f8d7a
mcrucovschii <mcrucovschii@gmail.com>
2022-05-27 17:56:40 +0200

    3

--- WebServer.tf
+++ WebServer.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "WebServer" {
 #!/bin/bash
 yum -y update
 yum -y install httpd
-myip='curl http://169.254.169.254/latest/meta-data/local-ipv4'
+myip=$('curl http://169.254.169.254/latest/meta-data/local-ipv4')
 echo "<h2>WebServer with ip: $myip</h2><br>Build by Terraform" > /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
