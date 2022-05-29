cadb9f40e1d6c931e9a0c0780b6855430cfc7d55
mcrucovschii <mcrucovschii@gmail.com>
2022-05-29 09:07:44 +0200

    4

--- WebServer.tf
+++ WebServer.tf
@@ -50,19 +50,15 @@ resource "aws_security_group" "WebServerSG" {
   }
 */
 
-  ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+  dynamic "ingress" {
+    for_each = ["22", "80", "443"]
+    content {
+      from_port        = ingress.value
+      to_port          = ingress.value
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
   }
 
   egress {
